7438f08d3b13416f7a294a4aff91be6555d55a25
Yu Qian <ray.qian@hotmail.com>
2020-03-09 15:23:29 -0500

    create MainTest.java and commit part of the unit test for Main class

---
+++ src/elevator/MainTest.java
@@ -0,0 +1,22 @@
+package elevator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	
+	@Test
+	public void testconvertSecondsToHMmSs() {
+		assertNotNull(Main.convertSecondsToHMmSs(180));
+		assertEquals("00:04:00",Main.convertSecondsToHMmSs(240000));
+	}
+	
+	@Test
+	public void testgetBuilding() throws InterruptedException {
+		Main.main(null);
+	//	assertNotNull(Main.sTime);
+	}
+
+}
